9b6479e84b329922aca68d04282a691ff79f0241
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-14 14:44:11 -0700

    Question and Answer models created, Rails_Models_assignment_16_completed

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:question) { Question.new(title: "New Question Title", body: "New Question Body", resolved: false)}
+  let(:answer) { Answer.new(body: "Answer Body", question: question) }
+
+  it "should respond to body" do
+    expect(answer).to respond_to(:body)
+  end  
+end
