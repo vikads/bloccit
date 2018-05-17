c525464b2e1ff4efc0e88d8f0fd293e3ba2bfb2b
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-16 17:09:54 -0700

    Unique post and unique comment added, Rails_Seeding_Data_assignment_18_completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,38 @@
 require 'random_data'
 
- # Create Posts
- 50.times do
- 
-   Post.create!(
+ # # Create Posts
+ # 50.times do
+ #
+ #   Post.create!(
+ #
+ #     title:  RandomData.random_sentence,
+ #     body:   RandomData.random_paragraph
+ #   )
+ # end
+ # posts = Post.all
+ #
+ # # Create Comments
+ #
+ # 100.times do
+ #   Comment.create!(
+ #
+ #     post: posts.sample,
+ #     body: RandomData.random_paragraph
+ #   )
+ # end
 
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "Very unique title", body: "More unique body")
+ puts "#{Post.count}"
 
- # Create Comments
+ puts "#{Comment.count}"
+ Comment.find_or_create_by(post_id: 101, body: "Very unique comment")
+ puts "#{Comment.count}"
 
- 100.times do
-   Comment.create!(
 
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+
+
+
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
