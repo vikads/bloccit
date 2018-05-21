8a4ff4777b7eeba4d48345e0eaed09e707deb896
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-21 10:54:10 -0700

    Rails_Seeding_Data_assignment_18_discussed_with_mentor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,32 @@
 require 'random_data'
 
- # # Create Posts
- # 50.times do
- #
- #   Post.create!(
- #
- #     title:  RandomData.random_sentence,
- #     body:   RandomData.random_paragraph
- #   )
- # end
- # posts = Post.all
- #
- # # Create Comments
- #
- # 100.times do
- #   Comment.create!(
- #
- #     post: posts.sample,
- #     body: RandomData.random_paragraph
- #   )
- # end
+ # Create Posts
+ 50.times do
+
+   Post.create!(
+
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
+
+ # Create Comments
+
+ 100.times do
+   Comment.create!(
+
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
 
  puts "#{Post.count}"
  post = Post.find_or_create_by(title: "Very unique title", body: "More unique body")
  puts "#{Post.count}"
 
  puts "#{Comment.count}"
+ #puts post
  Comment.find_or_create_by(post: post, body: "Very unique comment")
  puts "#{Comment.count}"
 
