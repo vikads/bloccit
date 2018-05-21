90a0df1f36e0e2abdae76ba12969a6f4fd084a89
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-21 14:13:19 -0700

    Index action, Rails_Introduction_to_CRUD_checkpoint_22_completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,33 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  #RSpec created a test for PostsController. type: :controller
+  #tells RSpec to treat the test as a controller test.
+  #This allows us to simulate controller actions such as HTTP requests.
+
+  let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "assigns [my_post] to @posts" do
+      get :index
+
+      expect(assigns(:posts)).to eq([my_post])
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
