e9b70804e0baf2eb394d02aaa2f255923a92951d
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-22 11:51:54 -0700

    The form_for Helper Method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,22 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+
+      flash[:notice] = "post was saved."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
   def edit
   end
 end
