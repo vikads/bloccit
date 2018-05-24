1a1b9a4f191d6aa74fb50b9f98419ec782ee6d1f
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-23 22:04:33 -0700

    Rails_CRUD_assignment_23_completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'random_data'
 
- Create Posts
+ # Create Posts
  50.times do
 
    Post.create!(
@@ -15,7 +15,7 @@ require 'random_data'
 
  100.times do
    Comment.create!(
- 
+
      post: posts.sample,
      body: RandomData.random_paragraph
    )
