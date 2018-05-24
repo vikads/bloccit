a2a607bd8bfd79e8a1058505bb8b1d365c7d8702
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-24 12:50:09 -0700

    edit Action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,5 +34,6 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe "POST create" do
+  describe "POST #create" do
     it "increases the number of Post by 1" do
       expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post, :count).by(1)
     end
@@ -70,12 +70,27 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to redirect_to Post.last
     end
   end
-  #
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+
+  describe "GET #edit" do
+     it "returns http success" do
+       get :edit, {id: my_post.id}
+       expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #edit view" do
+       get :edit, {id: my_post.id}
+       expect(response).to render_template :edit
+     end
+ 
+     it "assigns post to be updated to @post" do
+       get :edit, {id: my_post.id}
+
+       post_instance = assigns(:post)
+
+       expect(post_instance.id).to eq my_post.id
+       expect(post_instance.title).to eq my_post.title
+       expect(post_instance.body).to eq my_post.body
+     end
+   end
 
 end
