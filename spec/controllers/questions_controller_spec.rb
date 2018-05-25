ccae0a7425b73afe77e78992909b7b4f18704bf2
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-24 18:33:06 -0700

    Question Controllers, Seeds

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+include RandomData
+
+RSpec.describe QuestionsController, type: :controller do
+
+  let(:my_question) do
+    Question.create(
+      id: 1,
+      title: RandomData.random_sentence,
+      body: RandomData.random_paragraph,
+      resolved: false )
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "aasigns [my_question] to @questions" do
+      get :index
+      expect(assigns(:questions)).to eq([my_question])
+    end
+  end
+
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+end
