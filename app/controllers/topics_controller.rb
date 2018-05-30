16bce6bf93c58c47a3b0ad0efe96cc844d466d2f
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-30 15:13:32 -0700

    edit action

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -9,6 +9,24 @@ class TopicsController < ApplicationController
   end
 
   def new
-    @topic = Topic.new 
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
   end
 end
