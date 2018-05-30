69307666e5022f666a8af32689688ae717c4d7d7
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-30 12:20:34 -0700

    Topic Specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523175431) do
+ActiveRecord::Schema.define(version: 20180530180202) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 20180523175431) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "public",      default: true
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
