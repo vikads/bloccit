45921b35f55819c4af051103ce71f97769b8cf6f
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-30 12:59:20 -0700

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 require 'random_data'
 
+ # Create Topics
+ 15.times do
+   Topic.create!(
+     name: RandomData.random_sentence,
+     description: RandomData.random_sentence
+   )
+ end
+
  # Create Posts
  50.times do
 
@@ -51,6 +59,7 @@ require 'random_data'
 
 
  puts "Seed finished"
+ puts "#{Topic.count} topics created" 
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
  puts "#{Advertisement.count} ads created"
