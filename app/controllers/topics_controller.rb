33d88efa9a7dd34a2724ec9782dfd537e9b636d2
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-30 14:15:05 -0700

    Show Topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,4 +3,8 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
 end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe TopicsController, type: :controller do
   let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
-  describe "GET index" do
+  describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
@@ -12,6 +12,23 @@ RSpec.describe TopicsController, type: :controller do
     it "assigns my_topic to @topics" do
       get :index
       expect(assigns(:topics)).to eq([my_topic])
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, {id: my_topic.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_topic.id}
+      expect(assigns(:topic)).to eq(my_topic)
+    end
+
+    it "assigns my_topic to @topic" do
+      get :show, {id: my_topic.id}
+      expect(assigns(:topic)).to eq(my_topic)
     end
   end
 
