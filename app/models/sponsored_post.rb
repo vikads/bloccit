2d9ee7d8ed6ff5df9dba731c064b405de2ab4a90
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-31 14:19:55 -0700

    SponsoredPost controller

--- app/models/sponsored_post.rb
+++ app/models/sponsored_post.rb
@@ -1,4 +1,4 @@
 class SponsoredPost < ActiveRecord::Base
   belongs_to :topic
-  has_many :comments, dependent: :destroy
+  #has_many :comments, dependent: :destroy #not yet with comments
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Post, type: :model do
 
