7d5fb46cb41d009aa9674e9f96f23fc9e80230c6
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-06-02 09:27:24 -0700

    Rails_Authentication_User_Model_assignment_28_completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase if email.present? }
 
+  before_save :format_name
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -13,5 +15,16 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  def format_name
+    if name
+      name_arr = []
+      name.split.each do |part_name|
+        name_arr << part_name.capitalize
+      end
+      
+      self.name = name_arr.join(" ")
+    end
+  end
 
 end
