ea16139ebd6d8eba21132fccee1759e4ab216df9
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-06-21 00:29:58 +0100

    specs

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,7 @@
 class Vote < ActiveRecord::Base
+  belongs_to :user
+  belomgs_to :post
+
+  validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }, presence: true 
+
 end
--- db/migrate/20180619173413_create_votes.rb
+++ db/migrate/20180619173413_create_votes.rb
@@ -2,8 +2,8 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :value
-      t.reference :user
-      t.reference :post
+      t.references :user
+      t.references :post
 
       t.timestamps null: false
     end
