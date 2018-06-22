84226325d7ddd6eb6df9082cdbbca2ab06a17dae
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-06-22 12:26:33 +0100

    Implementing the Favorite model

--- db/migrate/20180619173413_create_votes.rb
+++ db/migrate/20180619173413_create_votes.rb
@@ -2,12 +2,11 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :value
-      t.references :user
-      t.references :post
+      t.references :user, index: true, foreign_key: true
+      t.references :post, index: true, foreign_key: true
 
       t.timestamps null: false
     end
-    add_index :votes, :user
-    add_index :votes, :post
+
   end
 end
