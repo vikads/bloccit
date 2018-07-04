3bc50f9c75c4ff3bf028d678fc864969f7e8b9d2
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-07-04 21:01:30 +0300

    Using the User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,4 +29,8 @@ class UsersController < ApplicationController
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
   end
+
+  def show
+    @user = User.find(params[:id])
+  end 
 end
